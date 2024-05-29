2720b5202016908d2789a8cb99b325828d3847b2
Awais Ahmad <itxawais211@gmail.com>
2024-05-28 20:37:35 -0700

    in this part i have learnt about how to manage database with ejs

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express') ;
+const app = express();
+const path = require('path') ;
+const userModel = require('./models/user');
+const user = require('./models/user');
+
+app.set("view engine" , "ejs") ;
+app.use(express.json());
+app.use(express.urlencoded({extended : true})) ;
+app.use(express.static(path.join(__dirname,"public")))
+
+app.get("/",(req,res)=>{
+    res.render("index")
+})
+app.get("/read",async (req,res)=>{
+    let users = await userModel.find()
+    res.render("read" , {users})
+})
+app.post("/create",async (req,res)=>{
+let {name,email,image} = req.body ;
+let users = await userModel.create({
+    name ,
+    email ,
+    image 
+})
+// console.log(users)
+res.redirect("/read")
+})
+app.get(`/delete:id`,async (req,res)=>{
+    await userModel.findOneAndDelete({_id:req.params.id})
+    res.redirect("/read")
+})
+app.get("/edit/:id",async(req,res)=>{
+ let user = await userModel.findOne({_id : req.params.id});
+ res.render("edit",{user})   
+})
+app.post("/update/:id",async (req,res)=>{
+    let {name,image,email} = req.body ;
+    let updatedUser = await userModel.findOneAndUpdate({_id : req.params.id},{name,image,email},{new:true});
+    res.redirect("/read");
+})
+app.listen(3000)
